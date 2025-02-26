74e52fc19a7dccdfc130570e7eaaff3efa4d869a
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2025-02-26 14:32:55 +0100

    Last deploy errors fixed

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,14 @@
 import HeroImage from "@/components/HeroImage/HeroImage";
-import Logo from "@/assets/logo.svg";
+import Logo from "@/assets/logo";
 import SignUpForm from "@/components/AuthForms/SignUpForm/SignUpForm";
 
 export default function SignUp(): React.ReactNode {
   return (
     <main className="h-full flex flex-col-reverse md:flex-row items-end justify-between p-7">
       <div className="w-full md:w-1/2 bg-[var(--tailor-blue)] rounded-lg md:mr-7 p-4 text-white">
-        <Logo className="mb-4 w-1/2 max-w-[194px]" />
+        <div className="mb-4 w-1/2 max-w-[194px]">
+          <Logo />
+        </div>
         <SignUpForm />
       </div>
       <HeroImage src="/images/login.jpeg" />
