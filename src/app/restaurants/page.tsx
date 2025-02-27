21b8ffe3a362d15753367a5f0527952bb4d5ce40
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2025-02-27 17:01:07 +0100

    Fixing deploy errors

--- src/app/restaurants/page.tsx
+++ src/app/restaurants/page.tsx
@@ -1,16 +1,11 @@
 import RestaurantsList from "@/components/RestaurantComponents/RestaurantsList/RestaurantsList";
 import CustomMap from "@/components/GoogleMapsAPI/CustomMap/CustomMap";
-import LoadingMap from "@/components/LoadingMap/LoadingMap";
 import RestaurantsListSkeleton from "@/components/RestaurantComponents/RestaurantsList/RestaurantsListSkeleton";
 
 import { Suspense } from "react";
 import { GogleMapsApiProvider } from "@/providers/GogleMapsApiProvider";
 
-export default function Restaurants({
-  children,
-}: {
-  children?: React.ReactNode;
-}): React.ReactNode {
+export default function Restaurants(): React.ReactNode {
   return (
     <main className="flex-1 flex flex-col gap-7 lg:flex-row justify-between overflow-y-auto">
       <div className="w-full h-full lg:w-1/2 rounded-xl overflow-hidden">
